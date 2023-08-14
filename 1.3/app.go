86f9e569384c72fd065f02a30f6e6a4c5b2ff243
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-08-14 14:54:56 +0500

    Упражнение 1.3

---
+++ 1.3/app.go
@@ -0,0 +1,34 @@
+// Измените программу так, чтобы она выводила также os.Args[0] имя выполняемой программы
+
+// Поэксперементируйте с измерением разницы времени выполнения
+// потенциально неэффективных версии и версии с применением strings.Join.
+
+// В разделе 1.6 демонстрируются часть пакета time.
+// А в разделе 11.4 - как написать тест производительности для ее систематической оценки.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	var s, sep string
+	//for i := 1; i < len(os.Args); i++ {
+	//	s += strconv.Itoa(i) + " \n" + sep + os.Args[i]
+	//
+	//}
+
+	start := time.Now()
+	for i := 0; i < len(os.Args); i++ {
+		//s += strings.Join(os.Args, sep) + " \n" // 700-1000
+		s += strconv.Itoa(i) + " \n" + sep + os.Args[i] // 400-550
+	}
+
+	m := time.Since(start).Nanoseconds()
+	fmt.Println(m)
+	fmt.Println("------------------------------------------------------")
+}
