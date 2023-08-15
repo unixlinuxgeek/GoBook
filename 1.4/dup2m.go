66ec40d3cbc17d0db14add6671ade9474b2ed03b
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-08-15 14:26:27 +0500

    Задача 1.4

---
+++ 1.4/dup2m.go
@@ -0,0 +1,43 @@
+// Упражнение 1.4
+// Измените программу dup2 так, чтобы она выводила имена всех файлов,
+// в которых найдены повторяющиеся строки.
+
+// Ищем комментарий 'Решение задачи 1.4 !!!'
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLinesN(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				continue
+			}
+			countLinesN(f, counts)
+			f.Close()
+		}
+		// Решение задачи 1.4 !!!
+		for s, i := range counts {
+			fmt.Println(i, s)
+		}
+	}
+}
+
+func countLinesN(file *os.File, counts map[string]int) {
+	input := bufio.NewScanner(file)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
